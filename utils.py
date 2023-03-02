ec7cc5ca5b7b1864fc811ec13429ed77ac20e7c6
burak <byrkbrk@gmail.com>
2023-03-02 19:49:35 +0300

    add crop function to utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
-# write here useful functions
+
+
+
+def crop(tensor_images, new_shape):
+    h = new_shape[2]
+    w = new_shape[3]
+    h_start = int((tensor_images.shape[2] - h) / 2)
+    w_start = int((tensor_images.shape[3] - w) / 2)
+    cropped_images = tensor_images[:, :, h_start:h_start+h, w_start:w_start+w]
+
+    return cropped_images
