1ff9804d4794f8438595f27f7b13bc68ad7e2cd5
burak <byrkbrk@gmail.com>
2023-10-08 17:52:32 +0300

    delete space

--- utils.py
+++ utils.py
@@ -9,7 +9,6 @@ def crop(tensor_images, new_shape):
     h_start = int((tensor_images.shape[2] - h) / 2)
     w_start = int((tensor_images.shape[3] - w) / 2)
     cropped_images = tensor_images[:, :, h_start:h_start+h, w_start:w_start+w]
-
     return cropped_images
 
 
